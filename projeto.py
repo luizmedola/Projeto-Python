9a684421c2fed5759740db4e0baf85b065a86901
Luiz Otávio <medolaluizotavio@gmail.com>
2025-09-15 16:04:31 -0400

    commit

---
+++ projeto.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome( 
+    service = webdriver.chrome.service.Service(ChromeDriverManager().install()
+                                               
+    ),
+
+    options=options
+)
+
+driver.get("https://www.google.com")
+
+time.sleep(2)
